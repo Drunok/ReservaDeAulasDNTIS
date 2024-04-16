a073fca77ea065cab23a9d149af5b9ca9e62e04f
Drunok <aguilarkevin825@gmail.com>
2024-04-16 03:12:27 -0400

    respuesta a solicitud rapida sin actualizar disponibilidad hora academica

--- backend/respuestaSolicitudRapida.php
+++ backend/respuestaSolicitudRapida.php
@@ -8,27 +8,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $data = json_decode(file_get_contents('php://input'));
 
-    //Informacion del formulario
-    $formData = $data->formData;
-
-    $nombreDocente = 'Leticia Blanco';
-    $capacidad = $formData->capacidad;
-    $fecha = $formData->fecha;
-    $horaInicial = $formData->hora;
-    $horaFinal = $formData->horaFinal;
-    $motivo = $formData->motivo;
-    $ambiente = $formData->ambiente;
-    $pendiente = 1;
-    $aprobado = 0;
-    $esUrgente = 0;
-
-    //Se valida que no haya campos vacios
-    if (empty($capacidad) || empty($fecha) || empty($horaInicial) || empty($horaFinal) || empty($motivo) || empty($ambiente)) {
-        http_response_code(400); // Bad Request
-        echo json_encode(['error' => 'Faltan campos requeridos en la solicitud']);
-        exit();
-    }
-
     //Conexion a la base de datos
     require 'newDBConnect.php';
     $newDB = new NewDBConnect();
@@ -59,9 +38,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     
 
-    function atenderSolicitudesPendientes($conn, $fecha, $horaInicial, $horaFinal)
+    function atenderSolicitudesPendientes($conn)
     {
         $numSolicitudesSinAtender = getNumSolicitudesSinAtender($conn);
+        $idsSolicitudesSinAtender = getIdsSolicitudesSinAtender($conn);
 
         $sql = "UPDATE solicitud SET revisionestapendiente = false WHERE revisionestapendiente = true";
         $stmt = $conn->prepare($sql);
@@ -70,50 +50,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $sqlAceptacion = "UPDATE solicitud SET solicitudfueaceptada = true WHERE solicitudfueaceptada = false";
         $stmtAceptacion = $conn->prepare($sqlAceptacion);
         $stmtAceptacion->execute();
-
-        // $sqlConfirmarAmbiente = "UPDATE periodo_academico_disponible SET estadisponible = false WHERE estadisponible = true AND fechadisponible = :fecha AND horainicial = :horaInicialFormulario AND horafinal = :horaFinalFormulario";
-        // $stmtConfirmarAmbiente = $conn->prepare($sqlConfirmarAmbiente);
-        // $stmtConfirmarAmbiente->bindParam(':fecha', $fecha);
-        // $stmtConfirmarAmbiente->bindParam(':horaInicialFormulario', $horaInicial);
-        // $stmtConfirmarAmbiente->bindParam(':horaFinalFormulario', $horaFinal);
-
-        // $stmtConfirmarAmbiente->execute();
-
-
+        
+        foreach ($idsSolicitudesSinAtender as $idSolicitud) {
+            $sqlGuardarRespuesta = "INSERT INTO respuesta_solicitud (idsolicitud, motivodenoreserva, fecharevision) VALUES (:idSolicitud, :motivonoreserva, :fecha)";
+            $stmtGuardarRespuesta = $conn->prepare($sqlGuardarRespuesta);
+            $stmtGuardarRespuesta->bindValue(':idSolicitud', $idSolicitud);
+            $stmtGuardarRespuesta->bindValue(':motivonoreserva', 'si se acepto');
+            $stmtGuardarRespuesta->bindValue(':fecha', date('Y-m-d H:i:s'));
+            $stmtGuardarRespuesta->execute();
+        }
+        return $numSolicitudesSinAtender;
     }
 
     function getNumSolicitudesSinAtender($conn) {
-        $sql = "SELECT COUNT(*) FROM solicitud WHERE revisionestapendiente = false";
+        $sql = "SELECT COUNT(*) FROM solicitud WHERE revisionestapendiente = true";
         $stmt = $conn->prepare($sql);
         $stmt->execute();
         $numSolicitudesSinAtender = $stmt->fetch(PDO::FETCH_ASSOC);
-        // echo json_encode($numSolicitudesSinAtender);
         return $numSolicitudesSinAtender['count'];
     }
 
     function getIdsSolicitudesSinAtender($conn) {
-        $sql = "SELECT idsolicitud FROM solicitud WHERE revisionestapendiente = false";
+        $sql = "SELECT idsolicitud FROM solicitud WHERE revisionestapendiente = true";
         $stmt = $conn->prepare($sql);
         $stmt->execute();
         $idsSolicitudesSinAtender = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $idsSolicitudesSinAtender = array_map(function($solicitud) {
             return $solicitud['idsolicitud'];
         }, $idsSolicitudesSinAtender);
-        echo json_encode($idsSolicitudesSinAtender);
+        return $idsSolicitudesSinAtender;
     }
 
-    
+    $solicitudesAtendidas = atenderSolicitudesPendientes($conn);
 
-    getIdsSolicitudesSinAtender($conn);
-    // $numSolicitudesSinAtender = getNumSolicitudesSinAtender($conn);
-    // echo json_encode($numSolicitudesSinAtender);
-
-
-
-
-    // getSolicitudes($conn);
-    // atenderSolicitudesPendientes($conn, $fecha, $horaInicial, $horaFinal);
-    // echo json_encode($fechasSolicitudesSinAtender);
-    // echo json_encode ($fechasSolicitudesSinAtender[0]['fechasolicitud']);
+    echo json_encode(['solicitudesAtendidas' => $solicitudesAtendidas]);
 
 }
